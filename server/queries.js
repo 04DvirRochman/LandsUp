0db35b52659aa45fcab83052e4d4dcded3538be6
Dan <nadav8.tch03@gmail.com>
2022-08-31 14:44:41 +0300

    server completed

--- server/queries.js
+++ server/queries.js
@@ -15,3 +15,34 @@ const sendQuery = async (query) => {
 const getFlights = async () => {
 	return await sendQuery({ text: 'SELECT * FROM t_flights' });
 };
+
+const getFlight = async (id) => {
+	return await sendQuery({ text: 'SELECT * FROM t_flights WHERE id=$1', values: [id] });
+};
+
+const deleteFlight = async (id) => {
+	return await sendQuery({ text: 'DELETE FROM t_flights WHERE id=$1', values: [id] });
+};
+
+const addFlight = async (flight) => {
+	await sendQuery({
+		text: 'INSERT INTO t_flights VALUES($1,$2,$3,$4,$5,$6,$7,$8)',
+		values: [
+			flight.id,
+			flight.name,
+			flight.origin,
+			flight.destination,
+			flight.departuretime,
+			flight.arrivaltime,
+			flight.airline,
+			flight.terminal,
+		],
+	});
+};
+
+module.exports = {
+	getFlights,
+	getFlight,
+	deleteFlight,
+	addFlight,
+};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,76 @@
 'use strict';
 const express = require('express');
 const shortid = require('shortid');
+const { getFlights, getFlight, addFlight, deleteFlight } = require('./queries');
 const PORT = 3000;
 const app = express();
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
-    res.send('test');
-    });
+app.get('/api/flights', async (req, res) => {
+	const flights = await getFlights();
+	if (!flights || !flights.length) {
+		res.status(404).send('Flights do not exist');
+	} else {
+		res.send(flights);
+	}
+});
+
+app.get('/api/flight/:id', async (req, res) => {
+	const id = req.params.id;
+	const flight = await getFlight(id);
+	if (flight.length === 0) {
+		res.status(404).send(`flight ${id} not found`);
+	} else {
+		res.send(flight[0]);
+	}
+});
+
+app.post('/api/flight', async (req, res) => {
+	const newFlight = {
+		id: shortid.generate(),
+		name: req.body.name,
+		origin: req.body.origin,
+		destination: req.body.destination,
+		departuretime: req.body.departuretime,
+		arrivaltime: req.body.arrivaltime,
+		airline: req.body.airline,
+		terminal: req.body.terminal,
+	};
+	if (
+		!newFlight.name ||
+		newFlight.name === '' ||
+		!newFlight.origin ||
+		newFlight.origin === '' ||
+		!newFlight.destination ||
+		newFlight.destination === '' ||
+		!newFlight.departuretime ||
+		newFlight.departuretime === '' ||
+		!newFlight.arrivaltime ||
+		newFlight.arrivaltime === '' ||
+		!newFlight.airline ||
+		newFlight.airline === '' ||
+		!newFlight.terminal ||
+		newFlight.terminal === ''
+	) {
+		res.status.send('invalid properties');
+	} else {
+		await addFlight(newFlight);
+		res.send(newFlight);
+	}
+});
+
+app.delete('/api/flight/:id', async (req, res) => {
+	const id = req.params.id;
+	const flight = await getFlight(id);
+	if (flight.length === 0) {
+		res.status(404).send(`Flight ${id} not found`);
+	} else {
+		await deleteFlight(id);
+		res.send(flight)[0];
+	}
+});
 
 app.listen(PORT, function (err) {
 	if (err) {
