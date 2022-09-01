8d39c59e0b24e351c44090015e8914b56033c925
04DvirRochman <nadav8.tch04@gmail.com>
2022-09-01 13:37:29 +0300

    started working list

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import FlightList from '../cmps/FlightList'
 import { getAll } from '../services.js'
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
 
 
 export default class Home extends Component {
@@ -8,24 +10,38 @@ export default class Home extends Component {
     constructor() {
         super();
         this.state = {
-            items: [],
-            loadingTasks: false
+            flights: [],
+            loadingFlights: false,
+            DetailsOpen: null
         }
     }
 
     componentDidMount() {
-        this.setState({ loadingTasks: true })
-        getAll().then(allTasks => {
-            this.setState({ tasks: allTasks })
-        }).finally(() => this.setState({ loadingTasks: false }))
+        this.setState({ loadingFlights: true })
+        getAll().then(allFlights => {
+            this.setState({ flights: allFlights })
+        }).finally(() => this.setState({ loadingFlights: false }))
+    }
+
+    openFilghtDetails=(id)=>{
+        this.setState({DetailsOpen: id});
     }
 
     render() {
-
+        const { flights, loadingFlights,DetailsOpen } = this.state;
+        if (loadingFlights) {
+            return;
+        }
+        if (flights.length < 1) {
+            return;
+        }
+        console.log(flights);
         return (
             <div>
-                hi
+                home
+                <FlightList flights={flights} DetailsOpen={DetailsOpen} openFilghtDetails={this.openFilghtDetails}/>
             </div>
+
         )
     }
 }
